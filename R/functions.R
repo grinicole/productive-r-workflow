b0398d1ae47a7da51cde2e4bec5c5772640b4465
Nicole Grinovero <nicolegrinovero@gmail.com>
2024-04-08 16:58:09 +0200

    Initial commit

---
+++ R/functions.R
@@ -0,0 +1,22 @@
+# plot function
+create_scatterplot <- function(data, selected_species, color, size) {
+  # Filter the data for the specified species and island
+  filtered_data <- data %>%
+    na.omit() %>%
+    filter(species == selected_species)
+
+  # Create the scatterplot
+  plot <- ggplot(
+    filtered_data,
+    aes(x = bill_length_mm, y = bill_depth_mm)
+  ) +
+    geom_point(color = color, size = size) +
+    labs(
+      x = "Bill Length (mm)",
+      y = "Bill Depth (mm)",
+      title = selected_species
+    ) +
+    theme(legend.position = "none")
+
+  return(plot)
+}
